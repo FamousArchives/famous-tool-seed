f622b7f32bc13ec321523c168686270620c1bf4a
Andrew de Andrade <andrew@deandrade.com.br>
2014-06-10 14:03:56 -0700

    Changed from 4 space indent to 2

--- test/bin.js
+++ test/bin.js
@@ -4,18 +4,18 @@ var spawn = require('child_process').spawn;
 var path = require('path');
 
 test('bin', function (t) {
-    t.plan(3);
+  t.plan(3);
 
-    var binPath = path.resolve(__dirname, '../bin/cmd.js');
-    var ps = spawn(binPath);
-    var stdout = '';
-    var stderr = '';
-    ps.stdout.on('data', function (buf) { stdout += buf; });
-    ps.stderr.on('data', function (buf) { stderr += buf; });
+  var binPath = path.resolve(__dirname, '../bin/cmd.js');
+  var ps = spawn(binPath);
+  var stdout = '';
+  var stderr = '';
+  ps.stdout.on('data', function (buf) { stdout += buf; });
+  ps.stderr.on('data', function (buf) { stderr += buf; });
 
-    ps.on('exit', function (code) {
-        t.equal(code, 0);
-        t.equal(stderr, '');
-        t.equal(stdout, 'argv: { _: [] }\nlib: module exports\n');
-    });
+  ps.on('exit', function (code) {
+    t.equal(code, 0);
+    t.equal(stderr, '');
+    t.equal(stdout, 'argv: { _: [] }\nlib: module exports\n');
+  });
 });
--- test/test.js
+++ test/test.js
@@ -4,7 +4,6 @@ var test = require('tape');
 var lib = require('../lib');
 
 test('base test', function (t) {
-    t.plan(1);
-
-    t.equal(lib, 'module exports');
+  t.plan(1);
+  t.equal(lib, 'module exports');
 });
